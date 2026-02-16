000682527be1c3560b8c8bdffc2a1e66d6f38522
markusplasser <markus.plasser8@gmail.com>
2026-02-16 21:33:30 +0100

    14.2 - GUI-update

--- Oberflaeche.c
+++ Oberflaeche.c
@@ -5,20 +5,73 @@
 #include "Calc.h"
 
 
-void start(struct ObjectArray *arr,int time_step) {
-    InitWindow(1000,1000,"Test");
+
+Vector* calc_Position(object_array *arr) {
+
+}
+
+
+void start(struct ObjectArray *arr, int time_step) {
+    InitWindow(1000, 1000, "Test");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
 
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        for (int i = 0; i < arr->count; i++) {
-            DrawCircle(arr->data[i].pos.data[0],arr->data[i].pos.data[1],arr->data[i].radius,RED);
+        ClearBackground(GRAY);
+
+        double y_middle = 0;
+        double x_middle = 0;
+        double max_y = 0;
+        double max_x = 0;
+        double max_distance_x = 0;
+        double max_distance_y = 0;
+
+        for (int j = 0; j < arr->count; j++) {
+            max_x += arr->data[j].pos.data[0];
+            max_y += arr->data[j].pos.data[1];
         }
+        y_middle = max_y / arr->count;
+        x_middle = max_x / arr->count;
+
+        for (int j = 0; j < arr->count; j++) {
+            if (arr->data[j].pos.data[0] > max_distance_x) {
+                max_distance_x = arr->data[j].pos.data[0] - x_middle;
+            }
+
+            if (arr->data[j].pos.data[1] > max_distance_y) {
+                max_distance_y = arr->data[j].pos.data[1] - y_middle;
+            }
+        }
+        if (max_distance_x < 0) {
+            max_distance_x *= -1;
+        }
+        if (max_distance_y < 0) {
+            max_distance_y *= -1;
+        }
+        double max_distance = max_distance_x > max_distance_y ? max_distance_x : max_distance_y;
+
+        for (int j = 0; j < arr->count; j++) {
+            double factor = max_distance / arr->data[j].pos.data[0];
+            double x_offset = factor * 400;
+            double pos_x = 500 + x_offset;
+
+            factor = max_distance / arr->data[j].pos.data[1];
+            double y_offset = factor * 400;
+            double pos_y = 500 + y_offset;
+
+
+            DrawCircle(pos_x, pos_y, arr->data[j].radius, RED);
+
+        }
+
+
         for (int i = 0; i < time_step; i++) {
-            calc_oneTick(arr,10);
+            calc_oneTick(arr, 10);
         }
         EndDrawing();
     }
+    CloseWindow();
 }
+
--- main.cu
+++ main.cu
@@ -9,9 +9,9 @@ extern "C" {
 ObjectArray arr;
 
 void add() {
-    object_array_add(&arr,create_andSetValue(20000000,20,create_Value_2D(500.0,500.0),create_Value_2D(0,0.005),create_Value_2D(0,0)));
-    object_array_add(&arr,create_andSetValue(10000000,20,create_Value_2D(450.0,500.0),create_Value_2D(0,-0.001),create_Value_2D(0,0)));
-    object_array_add(&arr,create_andSetValue(10000000,20,create_Value_2D(550.0,520.0),create_Value_2D(0,-0.001),create_Value_2D(0,0)));
+    object_array_add(&arr,create_andSetValue(20000000,20,create_Value_2D(300.0,700.0),create_Value_2D(0,0.001),create_Value_2D(0,0)));
+    object_array_add(&arr,create_andSetValue(10000000,20,create_Value_2D(800.0,600.0),create_Value_2D(0,-0.001),create_Value_2D(0,0)));
+    //object_array_add(&arr,create_andSetValue(10000000,20,create_Value_2D(400.0,220.0),create_Value_2D(0,-0.001),create_Value_2D(0,0)));
 }
 
 
@@ -21,7 +21,7 @@ int main() {
     add();
     object_array_print(&arr);
 
-    start(&arr,20);
+    start(&arr,2000);
 
     object_array_print(&arr);
 }
